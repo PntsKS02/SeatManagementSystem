6c8eb5962fea959e8523fc8c78b9dc616407de21
PntsKS02 <tadami.kiha40.2085@gmial.com>
2023-05-29 14:58:01 +0900

    upload-1

---
+++ src/main/java/com/example/demo/SeatMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatMangementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatMangementSystemApplication.class, args);
+	}
+
+}
